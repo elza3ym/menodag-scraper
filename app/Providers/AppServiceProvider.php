d6fcfe83818c6b0a2549fba01e1f5cb8d4e89e99
Mohamed Shehata <mohamed_king858@yahoo.com>
2022-08-17 20:16:08 +0200

    feat(scraper): add export and change pattern features

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use App\Jobs\ProcessData;
+use App\Models\Data;
+use App\Models\Session;
+use App\Models\Setting;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Queue\Events\JobFailed;
@@ -28,7 +31,29 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Queue::failing(function (JobFailed $event) {
-            var_dump($event);
+            $session = Session::where('is_running', 1)->first();
+            if (!$session) {
+                $oldSession = Session::latest()->first();
+                $session = new Session([
+                    'pattern' => Setting::all()?->first()?->pattern ?: env('SCRAPPER_PATTERN'),
+                    'is_running'    => 1,
+                    'time'      => 0,
+                ]);
+                if ($oldSession?->pattern === $session->pattern) {
+                    $session->current_number =  $oldSession->current_number;
+                }
+                $session->save();
+            }
+            if (!$session->current_number) {
+                $session->current_number = Data::getNumber($session->pattern);
+            } else {
+                $session->current_number++;
+            }
+            $session->save();
+            ProcessData::dispatch($session);
+            echo "===============================================================".PHP_EOL;
+            echo "Restarted".PHP_EOL;
+            echo "===============================================================".PHP_EOL;
         });
     }
 }
